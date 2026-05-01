d85efc5d99ee66ee964ae9de1a6df3fc70dcdf6b
stibu81 <slanz1137@gmail.com>
2026-05-01 17:15:33 +0200

    pass user agent to wikipedia

--- R/images.R
+++ R/images.R
@@ -5,6 +5,18 @@
 #'
 #' @param taxa character with the taxa for which an image URL should be
 #'  obtained.
+#' @param contact contact data to pass on in the user agent of the API call to 
+#'  Wikipedia. Starting in 2026, there is a strict rate limit on API calls per 
+#'  minute for calls without contact data. There are two possible ways to pass
+#'  contact data:
+#'  * a string containing an email address.
+#'  * a list that specifies an existing Wikipedia account. You need to specify two
+#'    named elements: "lang" giving the language code of the Wikipedia where the 
+#'    account is registered and "user" giving the user name.
+#'    Example: `list(lang = "de", user = "James Bond")`.
+#' 
+#'  See the [User Agent Policy](https://foundation.wikimedia.org/wiki/Policy:Wikimedia_Foundation_User-Agent_Policy)
+#'  for more information.
 #' @param size integer giving the width of the image in pixel.
 #' @param lang language of the Wikipedia page to use. Available languages
 #'  include "de" (German, the default), "en" (English), "fr" (French), and
@@ -20,14 +32,28 @@
 #' @export
 
 get_wikipedia_image_urls <- function(taxa,
-                                     size = 100,
-                                     lang = "de",
-                                     progress = TRUE) {
+                                      contact = NULL,
+                                      size = 120,
+                                      lang = "de",
+                                      progress = TRUE) {
+  user_agent <- get_user_agent_string(contact)
+  get_wikipedia_image_urls_(taxa, user_agent, size, lang, progress)
+}
+
+
+# workhorse function to download multiple image urls. It is need to allow the 
+# user facing functions to determine the user agent string.
+get_wikipedia_image_urls_ <- function(taxa,
+                                      user_agent = NULL,
+                                      size = 120,
+                                      lang = "de",
+                                      progress = TRUE,
+                                      error_call = rlang::caller_env()) {
 
   # make sure that size is a positive integer
   int_size <- suppressWarnings(as.integer(size))
   if (is.na(int_size) || size <= 0) {
-    cli::cli_abort("{size} is not a positive integer.")
+    cli::cli_abort("{size} is not a positive integer.", call = error_call)
   }
 
   # catch failure when connecting to wikipedia. A possible reason for failure
@@ -35,15 +61,20 @@ get_wikipedia_image_urls <- function(taxa,
   error_call <- rlang::current_call()
   tryCatch(
     suppressWarnings(
-      purrr::map_chr(taxa, get_wikipedia_image_url,
-                     size = int_size,
-                     lang = lang,
-                   .progress = progress)
+      purrr::map_chr(
+        taxa,
+        \(taxa) get_wikipedia_image_url(taxa,
+                                        user_agent = user_agent,
+                                        size = int_size,
+                                        lang = lang,
+                                        error_call = error_call),
+        .progress = progress)
     ),
     error = function(e) {
       cli::cli_abort(
         c(
           "x" = "Connection to {lang}.wikipedia.org failed.",
+          "i" = "Message: {e$parent$message}",
           "i" = paste("Check your internet connection and make sure,",
                       "\"{lang}\" is a valid Wikipedia language code."),
           ">" = paste0("Look up valid language codes ",
@@ -58,15 +89,18 @@ get_wikipedia_image_urls <- function(taxa,
 
 }
 
-get_wikipedia_image_url <- function(taxon, size, lang,
+get_wikipedia_image_url <- function(taxon, user_agent, size, lang,
                                     error_call = rlang::caller_env()) {
-
+  
   url <- paste0(
     "http://", lang, ".wikipedia.org/w/api.php?action=query&titles=",
     utils::URLencode(taxon),
     "&prop=pageimages&format=json&pithumbsize=", size, "&redirects="
   )
-  parsed <- jsonlite::fromJSON(url)$query$pages[[1]]
+  resp <- resp <- httr2::request(url) %>%
+      httr2::req_user_agent(user_agent) %>% 
+      httr2::req_perform()
+  parsed <- httr2::resp_body_json(resp)$query$pages[[1]]
   if ("thumbnail" %in% names(parsed)) {
     parsed$thumbnail$source
   } else {
@@ -85,6 +119,7 @@ get_wikipedia_image_url <- function(taxon, size, lang,
 #' @param retry Whether to retry images that have not been found previously.
 #' @param progress Whether to show a progress bar.
 #' @param quiet Should all output be suppressed?
+#' @inheritParams get_wikipedia_image_urls
 #'
 #' @return
 #' a `taxonomy_graph` with added image URLs. The file given by `file` is
@@ -94,6 +129,7 @@ get_wikipedia_image_url <- function(taxon, size, lang,
 
 enrich_taxonomy_with_images <- function(file,
                                         delim = ",",
+                                        contact = NULL,
                                         retry = FALSE,
                                         progress = TRUE,
                                         quiet = FALSE) {
@@ -107,15 +143,27 @@ enrich_taxonomy_with_images <- function(file,
   if (retry) image_url[image_url == "not_found"] <- NA_character_
 
   url_missing <- is.na(image_url)
-  if (!quiet) cat("try to get images for", sum(url_missing), "taxa.\n")
+  n_missing <- sum(url_missing)
+  if (n_missing == 0) {
+    cli::cli_alert_info("No missing images urls.")
+    return(taxonomy)
+  }
+
+  if (!quiet) {
+    cli::cli_alert_info("try to get images for {sum(url_missing)} taxa.")
+  }
+
+  user_agent <- get_user_agent_string(contact)
 
   # try in turns: common names (de), scientific names (de), scientific names (en)
   new_urls <- vertices$label[url_missing] %>%
-    insert_missing_image_urls(image_url[url_missing], "de", progress, "common names")
+    insert_missing_image_urls(
+      image_url[url_missing], user_agent, "de", progress, "common names"
+    )
   new_urls <- vertices$scientific[url_missing] %>%
-    insert_missing_image_urls(new_urls, "de", progress, "scientific names")
+    insert_missing_image_urls(new_urls, user_agent, "de", progress, "scientific names")
   new_urls <- vertices$scientific[url_missing] %>%
-    insert_missing_image_urls(new_urls, "en", progress, "English Wikipedia")
+    insert_missing_image_urls(new_urls, user_agent, "en", progress, "English Wikipedia")
 
   # If no URL was found, mark this as "not_found" to distinguish this from
   # taxa that have not yet been tried
@@ -136,10 +184,11 @@ enrich_taxonomy_with_images <- function(file,
 }
 
 
-insert_missing_image_urls <- function(taxa, image_url, lang, progress, label) {
+insert_missing_image_urls <- function(taxa, image_url, user_agent, lang, progress, label) {
   url_missing <- is.na(image_url)
-  image_url[url_missing] <- get_wikipedia_image_urls(
+  image_url[url_missing] <- get_wikipedia_image_urls_(
     taxa[url_missing],
+    user_agent = user_agent,
     progress = ifelse(progress, label, FALSE),
     lang = lang
   )
@@ -177,7 +226,7 @@ get_user_agent_string <- function(contact = NULL,
     )
   }
 
-  version <- packageVersion("simpleTaxonomy")
+  version <- utils::packageVersion("simpleTaxonomy")
 
   glue::glue("User-Agent: {bot_name}/{version} ({contact_str})")
 }
