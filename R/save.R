c3da9f1521f6ec5b4269f9141f383b24aa731225
stibu81 <slanz1137@gmail.com>
2026-07-26 12:28:39 +0200

    add custom css already in plot_taxonomy()

--- R/save.R
+++ R/save.R
@@ -14,14 +14,6 @@
 save_taxonomy <- function(widget, file,
                           background = "white",
                           title = "simpleTaxonomy") {
-
-  # prepare the css
-  css_file <- system.file("shinyApp", "www", "simpleTaxonomy.css",
-                          package = "simpleTaxonomy")
-  css <- readr::read_lines(css_file) %>%
-    paste(collapse = "\n")
-
   widget %>%
-    htmlwidgets::prependContent(htmltools::tags$style(css)) %>%
     htmlwidgets::saveWidget(file, background = background, title = title)
 }
